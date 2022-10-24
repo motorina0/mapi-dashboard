3bd0388cb35e5b76371e9e756a993df0d12b27a2
Vlad Stan <stan.v.vlad@gmail.com>
2022-10-24 11:02:32 +0300

    chore: code clean-up

--- src/components/core/NavBar.vue
+++ src/components/core/NavBar.vue
@@ -2,34 +2,7 @@
 
 <!-- <nav-bar /> in the DashboardView.vue -->
 <template>
-	<!-- app-bar props below customize bevaviotr and looks -->
-	<v-app-bar app color="primary" :clipped-left="true">
-		<!-- controls the sidebar colapsing-->
-		<v-app-bar-nav-icon @click="drawerButton" />
-		<router-link to="/">
-			<v-app-bar-title>
-				{{ title }}
-			</v-app-bar-title>
-		</router-link>
-		<v-divider class="mx-4" inset vertical></v-divider>
-		<!-- When placing a single v-spacer before or after the child components, 
-		the components will push to the right and left of its container -->
-		<v-spacer />
-		<MaterialfyNotifications :buttonColor="btnColor" />
-		<!-- userSnippet.vue menu -->
-
-		<v-menu nudge-bottom="57" :close-on-content-click="false">
-			<!-- recieving the "on" event lsitener -->
-			<template v-slot:activator="{ on }">
-				<v-avatar size="40" v-on="on" class="mx-2">
-					<img :src="getAvatar" alt="User Avatar" />
-				</v-avatar>
-			</template>
-			<MaterialfyUserSnippet />
-		</v-menu>
-
-		<MaterialfySettings :buttonColor="btnColor" />
-	</v-app-bar>
+	<div></div>
 </template>
 
 <script>
--- src/views/DashboardViews/Dash.vue
+++ src/views/DashboardViews/Dash.vue
@@ -61,74 +61,7 @@
 				<MaterialfyDataTable />
 			</v-col>
 		</v-row>
-		<!-- SECOND ROW  -->
-		<!-- Used named slots and props to overide the default card content below -->
-		<v-row class="d-flex align-start justify-center mb-2">
-			<!-- FIRST COLOR CARD with default content -->
-			<v-col md="4" class="my-2">
-				<MaterialfyColorCard
-					cardMaxWidth="500"
-					cardInnerText="Horse Power"
-					:cardShowDivider="false"
-					:cardShowActions="false"
-					cardColor="tertiary"
-				>
-					<template v-slot:crdInner>
-						<materialfy-apex-polar-map />
-					</template>
-				</MaterialfyColorCard>
-			</v-col>
-			<!-- SECOND COLOR CARD with secondary color -->
-			<v-col md="4">
-			
-			</v-col>
-			<v-col md="4">
-				<MaterialfyColorCard
-					cardMaxWidth="500"
-					cardInnerText="Wallet Type"
-					:cardShowDivider="false"
-					:cardShowActions="false"
-					:cardShowInnerText="false"
-					cardColor="accent"
-				>
-					<template v-slot:crdInner>
-						<materialfy-apex-multiple-radial-bars />
-					</template>
-				</MaterialfyColorCard>
-			</v-col>
-		</v-row>
 
-		<!-- THIRD ROW -->
-		<!-- Used named slots and props to overide the default card content below -->
-		<v-row class="d-flex align-start mb-2">
-			<v-col>
-				<MaterialfyColorCard cardInnerText="$$$$$$$ emoji" :cardShowDivider="false" :cardShowActions="false">
-					<template v-slot:crdInner>
-						<materialfy-apex-y-axis />
-					</template>
-				</MaterialfyColorCard>
-			</v-col>
-			<v-col>
-				<MaterialfyColorCard
-					cardInnerText="User Engagement"
-					:cardShowDivider="false"
-					:cardShowActions="false"
-					cardColor="secondary"
-				>
-					<template v-slot:crdInner>
-						<materialfy-apex-line-graph />
-					</template>
-				</MaterialfyColorCard>
-			</v-col>
-		</v-row>
-		<!-- FOURTH ROW with calendar -->
-		<v-row class="ma-3 d-flex align-start justify-center">
-			<v-col md="9">
-				<v-responsive :aspect-ratio="16 / 9">
-					<MaterialfyCalendar />
-				</v-responsive>
-			</v-col>
-		</v-row>
 	</v-container>
 </template>
 
